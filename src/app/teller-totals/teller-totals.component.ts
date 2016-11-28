03750c428662b9aa82d8c15f9e7a1c8fff1dc04a
tomwood2@gmail.com <tomwood2@gmail.com>
2016-11-27 19:44:11 -0800

    fixup number inputs;some validation

--- src/app/teller-totals/teller-totals.component.ts
+++ src/app/teller-totals/teller-totals.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-declare var moment: any;  // make ts happy
+import * as moment from 'moment';
+
+//declare var moment: any;  // make ts happy
 
 @Component({
   selector: 'app-teller-totals',
