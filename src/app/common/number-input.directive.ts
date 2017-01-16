1698f4a1e7b1505d7b105aae645a1295cad93513
tomwood2@gmail.com <tomwood2@gmail.com>
2017-01-16 12:01:29 -0800

    keydown filtering in NumberInputDirective

--- src/app/common/number-input.directive.ts
+++ src/app/common/number-input.directive.ts
@@ -24,6 +24,34 @@ export class NumberInputDirective /*implements OnInit*/ {
   }
 */
 
+  @HostListener("keydown", ["$event"])
+  onInput(event): boolean {
+
+    if (event.key == "0" ||
+      event.key == "1" ||
+      event.key == "2" ||
+      event.key == "3" ||
+      event.key == "4" ||
+      event.key == "5" ||
+      event.key == "6" ||
+      event.key == "7" ||
+      event.key == "8" ||
+      event.key == "9" ||
+      event.key == "." ||
+      event.key == "-" ||
+      event.key == "ArrowLeft" ||
+      event.key == "ArrowRight" ||
+      event.key == "Home" ||
+      event.key == "End" ||
+      event.key == "Tab" ||
+      event.key == "Backspace" ||
+      event.key == "Delete") {
+        return true;
+    }
+
+    return false;
+  }
+
   @HostListener("keydown.Enter", ["$event.target.value"])
   onkeydown(value) {
 
